02644d6374255c376aed3f54423007fcce938377
t41y0u <vuthaiduong0607@gmail.com>
2021-04-28 13:59:43 +0700

    Add base commands and initial game framework

---
+++ src/commands/general/rules.ts
@@ -0,0 +1,24 @@
+import { Command } from '@structures';
+import { Message } from 'discord.js';
+import { RULES } from '@utils/constants';
+
+export default class extends Command {
+    constructor() {
+        super('rules', {
+            aliases: ['rules'],
+            description: {
+                content: 'Show the rules',
+                examples: ['\nRules.'],
+            },
+        });
+    }
+
+    async exec(message: Message) {
+        return message.channel.send(
+            this.client.embeds
+                .default()
+                .setTitle('⚔️ Race to 100 ⚔️')
+                .setDescription(`❯ **Number of players**: 1 - ∞.\n❯ **Rules**:\n${RULES}`)
+        );
+    }
+}
