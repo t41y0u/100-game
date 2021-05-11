7eb5e47a0e302f4ebd948a0f9827ad94bd96dc69
t41y0u <vuthaiduong0607@gmail.com>
2021-05-11 21:14:38 +0700

    Allow random bet for player

--- src/structures/Game.ts
+++ src/structures/Game.ts
@@ -71,8 +71,9 @@ export class Game {
     private users: User[];
     private timer: Timer;
     private pausing: boolean;
+    private bets: Collection<User, Array<number>>;
     private leaderboard: Collection<User, number>;
-    private regex = /^\[([0-9]{1,3}), ([0-9]{1,3})\]$/;
+    private regex = /^\[([0-9]{1,3}|r), ([0-9]{1,3}|r)\]$/;
     constructor(client: Client, channel: TextChannel, rounds = 5, time = 30000) {
         this.client = client;
         this.rounds = rounds;
@@ -163,16 +164,28 @@ export class Game {
         this.channel.send(
             `Round **#${this.round + 1}** of **${this.rounds}!**\nYou will have ${
                 this.time / 1000
-            } seconds to place your bet.\n**Please remember that the declare message has to follow the format \'[start, end]\' and contains nothing else.**\nUpon successfully receiving your bet, I will react to your message with a ✅.\nYou can change your bet anytime before the timer ends. Your last bet will be your final choice.`
+            } seconds to place your bet.\n**Please remember that the declare message has to follow the format '[start, end]' and contains nothing else.**\nIf you want to randomize your bet, use the letter \`r\` (E.g: \`[r, 69]\`).\nUpon successfully receiving your bet, I will react to your message with a ✅.\nYou can change your bet anytime before the timer ends. Your last bet will be your final choice.`
         );
+        this.bets = new Collection<User, Array<number>>();
         const collector = this.channel.createMessageCollector(
             async m => {
-                const match = m.content.match(this.regex);
+                let match = m.content.match(this.regex);
                 if (!match) return false;
-                const start = +match[1],
+                let start = +match[1],
                     end = +match[2];
+                if (isNaN(start)) {
+                    if (isNaN(end)) {
+                        const f = Math.floor(Math.random() * 101);
+                        const s = Math.floor(Math.random() * 101);
+                        (start = Math.min(f, s)), (end = Math.max(f, s));
+                    }
+                    start = Math.floor(Math.random() * end);
+                } else if (isNaN(end)) {
+                    end = Math.floor(Math.random() * (101 - start) + start);
+                }
                 if (start < 0 || end > 100 || start > end) return false;
                 await m.react('✅');
+                this.bets.set(m.author, [start, end]);
                 return true;
             },
             { time: this.time }
@@ -194,42 +207,38 @@ export class Game {
         if (!collected) {
             return this.channel.send('An unexpected error has occured.');
         }
-        let bets = new Collection<User, string>();
-        collected.forEach(m => bets.set(m.author, m.content));
         if (this.users.some(u => u.id === this.client.user.id)) {
-            const f = Math.floor(Math.random() * 100);
-            const s = Math.floor(Math.random() * 100);
+            const f = Math.floor(Math.random() * 101);
+            const s = Math.floor(Math.random() * 101);
             const start = Math.min(f, s),
                 end = Math.max(f, s);
-            bets.set(this.client.user, `[${start}, ${end}]`);
+            this.bets.set(this.client.user, [start, end]);
         }
-        const betsDisplay = bets.map((b, u) => `- **${u.tag}**: ${b}`);
+        const betsDisplay = this.bets.map((b, u) => `- **${u.tag}**: [${b.join(', ')}]`);
         this.channel.send(`Time's up! Here are the bets:\n${betsDisplay.join('\n')}`);
         this.timer.reset(async () => {
-            await this.pauseChamp(bets);
+            await this.pauseChamp();
         }, 1000);
     }
 
-    async pauseChamp(bets: Collection<User, string>) {
+    async pauseChamp() {
         const msg = await this.channel.send(`And the secret number is: ... PauseChamp`);
         this.timer.reset(async () => {
-            await this.announceSecretNumber(msg, bets);
+            await this.announceSecretNumber(msg);
         }, 5000);
     }
 
-    async announceSecretNumber(message: Message, bets: Collection<User, string>) {
-        const number = Math.floor(Math.random() * 100);
+    async announceSecretNumber(message: Message) {
+        const number = Math.floor(Math.random() * 101);
         message.edit(`And the secret number is: ${number}!`);
         this.timer.reset(() => {
-            this.calculateLeaderboard(bets, number);
+            this.calculateLeaderboard(number);
         }, 1000);
     }
 
-    calculateLeaderboard(bets: Collection<User, string>, number: number) {
-        bets.forEach((b, u) => {
-            const match = b.match(this.regex);
-            const start = +match[1],
-                end = +match[2];
+    calculateLeaderboard(number: number) {
+        this.bets.forEach((b, u) => {
+            const [start, end] = b;
             if (start <= number && number <= end) {
                 const cur = this.leaderboard.get(u);
                 this.leaderboard.set(u, cur + 100 - (end - start + 1));
